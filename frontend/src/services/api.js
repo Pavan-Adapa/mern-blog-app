f5853debfd8dfbb7b92a0035c9e30f4300ab5f78
Pavan-Adapa <padapa102@gmail.com>
2026-06-11 20:41:04 +0530

    Added backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "backend:https://mern-blog-app-vmwq.onrender.com",
   withCredentials: true, // 👈 THIS IS REQUIRED
   headers: {
     "Content-Type": "application/json",
