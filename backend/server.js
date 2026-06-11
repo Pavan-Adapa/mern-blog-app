ec2a7f627f0e0a099cc89eb2c757202e6764d1de
Pavan-Adapa <padapa102@gmail.com>
2026-06-11 20:38:24 +0530

    Added backend url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend
+    origin: "https://mern-blog-app-beta-one.vercel.app/", // your frontend
     credentials: true,
   }),
 );
