078a5dd630f808c49be6282b77257c3d0a70f097
Pavan-Adapa <padapa102@gmail.com>
2026-06-11 20:46:54 +0530

    Rectified url mistakes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "backend:https://mern-blog-app-vmwq.onrender.com",
+  baseURL: "https://mern-blog-app-vmwq.onrender.com",
   withCredentials: true, // 👈 THIS IS REQUIRED
   headers: {
     "Content-Type": "application/json",
